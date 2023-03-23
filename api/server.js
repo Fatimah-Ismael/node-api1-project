c753bd1c2ddad20693c2e839849ca26ed0d900e5
Fatimah Ismael <fatimahismael@yahoo.com>
2023-03-23 12:48:37 -0700

    completed

--- api/server.js
+++ api/server.js
@@ -2,6 +2,46 @@
 const express = require('express')
 const server = express();
 const User = require('./users/model')
+server.use(express.json())
+
+server.put('/api/users/:id', async (req, res)=>{
+    try{
+        const possibleUser = await User.findById(req.params.id)
+    if (!possibleUser){
+        res.status(404).json({
+            message: 'The user with the specified ID does not exist'
+        })
+    }else{
+        const user = req.body
+        if(!user.name || !user.bio){
+            res.status(400).json({
+                message: "Please provide name and bio for the user" 
+            })
+        }else{
+            const updatedUser = await User.update(req.params.id, req.body)
+            res.status(200).json(updatedUser)
+        }
+    }
+
+    }catch(err){
+        res.status(500).json({
+            message:'error updating user'
+        })
+    }
+    
+})
+
+server.delete('/api/users/:id',async(req, res)=>{ const possibleUser = await User.findById(req.params.id)
+    if(!possibleUser){
+        res.status(404).json({
+            message: 'The user with the specified ID does not exist',
+        })
+    } else{
+        const deletedUser = await User.remove(possibleUser.id)
+        res.status(200).json(deletedUser)
+    }
+   
+    })
 
 server.get('/api/users', async(req, res)=>{
     User.find()
@@ -33,15 +73,18 @@ server.get('/api/users/:id', (req, res)=>{
     })
 })
 server.post('/api/users', (req, res)=>{
-    User.update()
-    const {name, bio}= req.body
-    .then(users=>{
-        if(!name || !bio){
-            res.status(400).json({
-                 message: "Please provide name and bio for the user"
-            })
-        }
-        res.status(201).json(users)
+    const user= req.body
+    if(!user.name || !user.bio){
+        res.status(400).json({
+             message: "Please provide name and bio for the user"
+        })
+    }else{
+
+    
+    User.insert(user)
+    .then(createdUser=>{
+       
+        res.status(201).json(createdUser)
     })
     .catch(err=> {
         res.status(500).json({
@@ -49,6 +92,7 @@ server.post('/api/users', (req, res)=>{
             err: err.message,
         })
     })
+}
 })
 
 server.use('*', (req, res)=>{
