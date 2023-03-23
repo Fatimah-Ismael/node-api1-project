004faf714338a7565b3b73de2f6b61f708273e0a
Fatimah Ismael <fatimahismael@yahoo.com>
2023-03-23 11:20:45 -0700

    work in progress

--- api/server.js
+++ api/server.js
@@ -1,3 +1,60 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const server = express();
+const User = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get('/api/users', async(req, res)=>{
+    User.find()
+    .then( users => {
+       // throw new Error('OMG!!')
+       res.status(200).json(users)
+    })
+    .catch(err=> {
+        res.status(500).json({
+            message: 'Error accessing users',
+            err: err.message,
+        })
+    })
+})
+server.get('/api/users/:id', (req, res)=>{
+    User.findById(req.params.id)
+    .then(user=>{
+        if(!user){
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        }
+        res.json(user)
+    }).catch(err=>{
+        res.status(500).json({
+            message:'error getting user ID',
+            err: err.message,
+        })
+    })
+})
+server.post('/api/users', (req, res)=>{
+    User.update()
+    const {name, bio}= req.body
+    .then(users=>{
+        if(!name || !bio){
+            res.status(400).json({
+                 message: "Please provide name and bio for the user"
+            })
+        }
+        res.status(201).json(users)
+    })
+    .catch(err=> {
+        res.status(500).json({
+            message:'Error creating new user',
+            err: err.message,
+        })
+    })
+})
+
+server.use('*', (req, res)=>{
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+
+
 const server = require('./api/server');
 
 const port = 9000;
 
 // START YOUR SERVER HERE
+server.listen(9000, () => console.log('API running on port 9000'));
